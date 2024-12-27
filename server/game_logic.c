73e76c298eb9699a1f74fe72e0c92f60d373d999
kiki <lioukiki@gmail.com>
2024-12-27 22:07:19 +0800

    refactor: modulize input_player_combination function for maintainability

--- server/game_logic.c
+++ server/game_logic.c
@@ -31,18 +31,22 @@ struct Player {
 };
 
 struct Card deck_num[44];
+struct Player pc[MAX_PLAYERS];
 char deck_op[5] = {'+', '-', '*', '/', 'R'};
 int random_array[44];
 int clientFd[MAX_PLAYERS];
 int deal_index = 0;
+int player_count = 0;
 
 void Initialize_random_array();
 void Initialize_card();
-void deal_cards(struct Player *player);
-void handle_betting_phase(struct Player *players, int num_players, int *main_pot);
+void deal_cards(int player_index);
+void handle_betting_phase(int *main_pot);
 void broadcast_message(const char *message, int num_players);
 double evaluate_expression(const char *expression);
-void determine_winners(struct Player *players, int num_players, int *main_pot);
+void determine_winners(int *main_pot);
+void Initialize_player();
+void start_game();
 
 void Initialize_random_array() {
     for (int i = 0; i < 44; i++) {
@@ -68,33 +72,35 @@ void Initialize_card() {
     }
 }
 
-void deal_cards(struct Player *player) {
+void deal_cards(int player_index) {
+    // Deal 4 cards and 3 operators to each player
     for (int i = 0; i < 4; i++) {
         int n = random_array[deal_index++];
-        player->card[i] = deck_num[n];
+        pc[player_index].card[i] = deck_num[n];
     }
     for (int i = 0; i < 3; i++) {
-        player->op[i] = deck_op[rand() % 4];
+        pc[player_index].op[i] = deck_op[rand() % 4];
     }
-    if (rand() % 5 == 0) {
-        player->op[3] = deck_op[4];
+    if (rand() % 5 == 0) {  // 20% chance to get an root operator
+        pc[player_index].op[3] = deck_op[4];
     } 
 }
-void handle_betting_phase(struct Player *players, int num_players, int *main_pot) {
+
+void handle_betting_phase(int *main_pot) {
     int max_bet = 0;
-    for (int i = 0; i < num_players; i++) {
-        if (players[i].folded) continue;
+    for (int i = 0; i < player_count; i++) {
+        if (pc[i].folded) continue;
 
         char buffer[BUFFER_SIZE];
         snprintf(buffer, sizeof(buffer), "Player %d, it's your turn to bet. Max bet: %d, your chips: %d.\n",
-                 players[i].id, max_bet, players[i].chips);
+                 pc[i].id, max_bet, pc[i].chips);
         write(clientFd[i], buffer, strlen(buffer));
 
         read(clientFd[i], buffer, sizeof(buffer));
 
         if (strncmp(buffer, "fold", 4) == 0) {
-            players[i].folded = true;
-            snprintf(buffer, sizeof(buffer), "Player %d folded. You are out of this round.\n", players[i].id);
+            pc[i].folded = true;
+            snprintf(buffer, sizeof(buffer), "Player %d folded. You are out of this round.\n", pc[i].id);
             write(clientFd[i], buffer, strlen(buffer));
             continue;
         }
@@ -102,28 +108,28 @@ void handle_betting_phase(struct Player *players, int num_players, int *main_pot
         int bet_high = 0, bet_low = 0;
         sscanf(buffer, "%d %d", &bet_high, &bet_low);
 
-        if (bet_high < max_bet && bet_high != players[i].chips) {
+        if (bet_high < max_bet && bet_high != pc[i].chips) {
             snprintf(buffer, sizeof(buffer), "Invalid high bet! You must bet more than %d or go All-In.\n", max_bet);
             write(clientFd[i], buffer, strlen(buffer));
             i--;
             continue;
         }
 
-        if (bet_high > players[i].chips) bet_high = players[i].chips;
+        if (bet_high > pc[i].chips) bet_high = pc[i].chips;
 
-        players[i].chips -= bet_high;
-        players[i].bet_high = bet_high;
+        pc[i].chips -= bet_high;
+        pc[i].bet_high = bet_high;
         max_bet = bet_high > max_bet ? bet_high : max_bet;
         *main_pot += bet_high;
 
-        if (bet_low > players[i].chips) bet_low = players[i].chips;
+        if (bet_low > pc[i].chips) bet_low = pc[i].chips;
 
-        players[i].chips -= bet_low;
-        players[i].bet_low = bet_low;
+        pc[i].chips -= bet_low;
+        pc[i].bet_low = bet_low;
         *main_pot += bet_low;
 
         snprintf(buffer, sizeof(buffer), "You bet %d chips on high and %d chips on low. Remaining chips: %d.\n",
-                 bet_high, bet_low, players[i].chips);
+                 bet_high, bet_low, pc[i].chips);
         write(clientFd[i], buffer, strlen(buffer));
     }
 }
@@ -194,6 +200,7 @@ double evaluate_expression(const char *expression) {
                     break;
                 case '/':
                     numbers[i] = numbers[i] / (numbers[i + 1] != 0 ? numbers[i + 1] : 1);
+                    // TODO: Handle division by zero, maybe need to tell the player to re-enter the expression
                     break;
             }
             for (int j = i + 1; j < num_index - 1; j++) {
@@ -222,22 +229,24 @@ double evaluate_expression(const char *expression) {
 
     return result;
 }
-void determine_winners(struct Player *players, int num_players, int *main_pot) {
+
+void determine_winners(int *main_pot) {
     double best_high = -1e9, best_low = 1e9;
     int winner_high = -1, winner_low = -1;
 
-    for (int i = 0; i < num_players; i++) {
-        if (players[i].folded) continue;
+    for (int i = 0; i < player_count; i++) {
+        if (pc[i].folded) continue;
 
-        double result_high = evaluate_expression(players[i].expression_high);
-        double result_low = evaluate_expression(players[i].expression_low);
+        double result_high = evaluate_expression(pc[i].expression_high);
+        double result_low = evaluate_expression(pc[i].expression_low);
 
-        if (players[i].bet_high > 0 && fabs(result_high - 20) < fabs(best_high - 20)) {
+
+        if (pc[i].bet_high > 0 && fabs(result_high - 20) < fabs(best_high - 20)) {
             best_high = result_high;
             winner_high = i;
         }
 
-        if (players[i].bet_low > 0 && fabs(result_low - 1) < fabs(best_low - 1)) {
+        if (pc[i].bet_low > 0 && fabs(result_low - 1) < fabs(best_low - 1)) {
             best_low = result_low;
             winner_low = i;
         }
@@ -246,38 +255,32 @@ void determine_winners(struct Player *players, int num_players, int *main_pot) {
     char buffer[BUFFER_SIZE];
     snprintf(buffer, sizeof(buffer), "Winner (High): Player %d\nWinner (Low): Player %d\n",
              winner_high + 1, winner_low + 1);
-    broadcast_message(buffer, num_players);
+    broadcast_message(buffer, player_count);
 
     if (winner_high != -1) {
-        players[winner_high].chips += players[winner_high].bet_high * 2; // High bet payout
+        pc[winner_high].chips += pc[winner_high].bet_high * 2; // High bet payout
     }
 
     if (winner_low != -1) {
-        players[winner_low].chips += players[winner_low].bet_low * 2; // Low bet payout
+        pc[winner_low].chips += pc[winner_low].bet_low * 2; // Low bet payout
     }
 
     *main_pot = 0; // Reset main pot after payout
 }
 
-void start_game(int players) {
-    Initialize_card();
-    Initialize_random_array();
-
-    struct Player pc[players];
-    for (int i = 0; i < players; i++) {
+void Initialize_player() {
+    for (int i = 0; i < player_count; i++) {
         pc[i].id = i + 1;
         pc[i].chips = 100; 
         pc[i].folded = false;
         pc[i].bet_high = 0;
         pc[i].bet_low = 0;
-        deal_cards(&pc[i]);
+        deal_cards(i);
     }
+}
 
-    int main_pot = 0;
-
-    handle_betting_phase(pc, players, &main_pot);
-
-    for (int i = 0; i < players; i++) {
+void input_player_combination() {
+    for (int i = 0; i < player_count; i++) {
         if (pc[i].folded) continue;
 
         char buffer[BUFFER_SIZE];
@@ -288,15 +291,23 @@ void start_game(int players) {
         read(clientFd[i], buffer, sizeof(buffer));
         sscanf(buffer, "%s %s", pc[i].expression_high, pc[i].expression_low);
     }
-
-    determine_winners(pc, players, &main_pot);
 }
 
+void start_game() {
+    int main_pot = 0;
+    Initialize_card();  // Initialize deck of cards with index
+    Initialize_random_array();  // Initialize random array for card shuffling
+    Initialize_player();
+    handle_betting_phase(&main_pot);
+    input_player_combination();
+    determine_winners(&main_pot);
+}
 
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc - 1; i++) {
         clientFd[i] = atoi(argv[i + 1]);
     }
-    start_game(MAX_PLAYERS);
+    player_count = argc - 1;
+    start_game();
     return 0;
 }
